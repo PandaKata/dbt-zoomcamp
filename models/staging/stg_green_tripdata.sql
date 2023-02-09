78d455feccc239318edc2c4d3b4564fa78684817
kataeichinger@gmail.com <kataeichinger@gmail.com>
2023-02-09 22:44:44 +0000

    added package

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -3,6 +3,7 @@
 
 select 
 -- identifiers
+    {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
     cast(ratecodeid as integer) as ratecodeid,
     cast(pulocationid as integer) as  pickup_locationid,
@@ -32,4 +33,10 @@ select
     cast(congestion_surcharge as numeric) as congestion_surcharge
 
 from {{ source('staging', 'green_tripdata') }}
-limit 100
+where vendorid is not null
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
